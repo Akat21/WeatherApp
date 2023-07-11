476c99dbdb45ab2f620af42fa98070a803b34954
Grzegorz Sawinski <sawinskigrzesiu@gmail.com>
2023-07-11 19:06:26 +0200

    filled with data

--- App.js
+++ App.js
@@ -4,19 +4,41 @@ const API_CITIES_KEY = '565816454eb44f05a8074d3e6a545669'
 const API_CITIES_URL = 'https://api.geoapify.com/v1/geocode/autocomplete?text='
 
 const App = () =>{
-    let availableCities = [];
+    let availableCities = []
+    let weatherData = {}
+    const dateOptions = {weekday: 'long', year: 'numeric', month: 'long', day:'numeric'}
     const searchInput = document.querySelector('.search-input')
     const searchDiv = document.querySelector('.search-autocomplete')
 
     //Fetch Weather
-    const fetchWeather = async(url) =>{
+    const fetchWeather = async(city) =>{
         try{
-            const response = await fetch(API_WEATHER_URL + url)
+            const response = await fetch(API_WEATHER_URL + `locations=${city}&aggregateHours=24&unitGroup=us&shortColumnNames=false&contentType=json&key=${API_WEATHER_KEY}`)
             const data = await response.json()
+            weatherData = {...data.locations}
+            
+            //Set Data
+            document.querySelector('.location-details h2').textContent = Object.keys(weatherData)
+            const date = new Date(weatherData[Object.keys(weatherData)].values[0].datetime)
+            const temp = Math.floor((5/9) * (weatherData[Object.keys(weatherData)].values[0].temp - 32))
+            const icon = weatherData[Object.keys(weatherData)].currentConditions.icon
+            const cloudCover = Math.floor(weatherData[Object.keys(weatherData)].values[0].cloudcover)
+            const humidity = Math.floor(weatherData[Object.keys(weatherData)].values[0].humidity)
+            const wind = Math.floor(weatherData[Object.keys(weatherData)].values[0].wspd)
+            const rain = weatherData[Object.keys(weatherData)].values[0].precip
+            document.querySelector('.location-details p').textContent = date.toLocaleDateString('en-US', dateOptions)
+            document.querySelector('.display h1').textContent = `${temp}\u00B0`
+            document.querySelector('.display img').src = `./assets/SVG/1st Set - Color/${icon}.svg`
+            document.querySelector('.detail-param-cloudy .val').textContent = `${cloudCover}%`
+            document.querySelector('.detail-param-humidity .val').textContent = `${humidity}%`
+            document.querySelector('.detail-param-wind .val').textContent = `${wind} km/h`
+            document.querySelector('.detail-param-rain .val').textContent = `${rain} mm`
+
         } catch(err) {
             console.error(err)
         }
     }
+    fetchWeather('Warsaw')
 
     //Fetch Cities
     const fetchCities = async(url) =>{
@@ -43,7 +65,6 @@ const App = () =>{
 
         //Search for Weather and Cities 
         const searchText = event.target.value;
-        fetchWeather(`locations=${searchText}&aggregateHours=24&unitGroup=us&shortColumnNames=false&contentType=json&key=${API_WEATHER_KEY}`)
         if(searchText.length > 0){
             fetchCities(`${searchText}&apiKey=${API_CITIES_KEY}`)
         } else { 
@@ -55,6 +76,20 @@ const App = () =>{
             const button = document.createElement('button')
             button.textContent = availableCities[idx]
             searchDiv.appendChild(button)
+        })
+
+        //Choose of City
+        const cityBtns = document.querySelectorAll('.search-autocomplete button')
+        cityBtns.forEach((btn) =>{
+            btn.addEventListener('click', function(){
+                const city = btn.textContent
+                availableCities = []
+                while (searchDiv.firstChild) {
+                    searchDiv.removeChild(searchDiv.firstChild);
+                }
+                searchInput.value = ""
+                fetchWeather(city)
+            })
         })
     })
 }
